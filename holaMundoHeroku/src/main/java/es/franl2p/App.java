3fd4c87821a2a970439980b95211ed66cff4522e
Fran Paredes <franciscoluis.paredes@gmail.com>
2016-05-04 13:14:21 +0200

    Configuración Heroku
    Se prepara la aplicación para que lea y configure el puerto asignado por
    Heroku.

--- holaMundoHeroku/src/main/java/es/franl2p/App.java
+++ holaMundoHeroku/src/main/java/es/franl2p/App.java
@@ -1,5 +1,7 @@
 package es.franl2p;
 
+import static spark.Spark.*;
+
 import es.franl2p.controller.CarsController;
 import es.franl2p.controller.HolaMundoController;
 
@@ -8,10 +10,23 @@ import es.franl2p.controller.HolaMundoController;
  *
  */
 public class App {
+	
 	public static void main(String[] args) {
+		// Configurate the Heroku App
+		configureHerokuApp();
 		
 		// Creates the controllers
 		new HolaMundoController();
 		new CarsController();
 	}
+
+    static void configureHerokuApp() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        
+        // If there's a PORT assigned by Heroku we set it.
+        if (processBuilder.environment().get("PORT") != null) {
+            port(Integer.parseInt(processBuilder.environment().get("PORT")));
+        }
+    }
+    
 }
