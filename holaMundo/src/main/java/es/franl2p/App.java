565225d3fbe35b47e14738c9257df6b70f6a864e
Francisco Luis Paredes <franciscoluis.paredes@gmail.com>
2017-02-24 17:32:52 +0100

    Añadir logger
    Se añade el logger

--- holaMundo/src/main/java/es/franl2p/App.java
+++ holaMundo/src/main/java/es/franl2p/App.java
@@ -2,12 +2,24 @@ package es.franl2p;
 
 import static spark.Spark.*;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import spark.route.RouteOverview;
+
 public class App {
+	private final static Logger logger = LoggerFactory.getLogger(App.class);
+	
 	public static void main( String[] args ) {
-//		get("/hola", (req, res) -> "¡Hola Mundo!");
+		RouteOverview.enableRouteOverview();
+
+		get("/", (req, res) -> "App running");
+		
+		get("/hola", (req, res) -> "¡Hola Mundo!");
 		
 		get("/hola/:nombre", (req, res) -> {
 			String hola = "¡Hola " + req.params(":nombre") + "!";
+			logger.info("hola: " + hola);
 			return hola;
 		});
 		
@@ -23,8 +35,20 @@ public class App {
 					res.redirect("/hola/" + nombre);
 				}
 			}
+			logger.info("respuesta: " + respuesta);
 			
 			return respuesta;
+		});
+		
+		get("/stop", (req, res) -> {
+			logger.info("Stopping App...");
+			stop();
+			return "Stopping App...";
+		});
+		
+		// Rutas no definidas
+		notFound((req, res) -> {
+		    return "<html><body><h1>Error 404</h1><p>La página " + req.url() + " solicitada no existe</p></body></html>";
 		});
     }
 }
