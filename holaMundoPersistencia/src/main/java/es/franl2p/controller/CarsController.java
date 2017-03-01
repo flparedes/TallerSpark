cdb0acfb0c66fca7579ce99114d991f8356335ba
Francisco Luis Paredes <franciscoluis.paredes@gmail.com>
2017-03-01 20:50:34 +0100

    Cambios controller
    Cambios en el controller para guardar coches y fabricantes

--- holaMundoPersistencia/src/main/java/es/franl2p/controller/CarsController.java
+++ holaMundoPersistencia/src/main/java/es/franl2p/controller/CarsController.java
@@ -5,6 +5,9 @@ import static spark.Spark.*;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import es.franl2p.daos.CarDao;
 import es.franl2p.daos.ManufacturerDao;
 import es.franl2p.model.Car;
@@ -13,6 +16,7 @@ import spark.ModelAndView;
 import spark.template.freemarker.FreeMarkerEngine;
 
 public class CarsController {
+	private final static Logger logger = LoggerFactory.getLogger(CarsController.class);
 	
 	private CarDao carDao = new CarDao();
 	private ManufacturerDao manufacturerDao = new ManufacturerDao();
@@ -32,73 +36,101 @@ public class CarsController {
 	    }, new FreeMarkerEngine());
 		 
 		/**
-		 * Ruta del formulario de pruebas
+		 * Ruta del formulario de fabricante
 		 */
-		get("/form", (req, res) -> {
+		get("/manufacturer/new", (req, res) -> {
 			Map<String, Object> attributes = new HashMap<String, Object>();
 
-	        attributes.put("coches", carDao.findAll());
+	        attributes.put("textoCabecera", "Nuevo fabricante");
+	        attributes.put("manufacturers", manufacturerDao.findAll());
 	
-	        return new ModelAndView(attributes, "formulario.ftl");
+	        return new ModelAndView(attributes, "formularioManufacturer.ftl");
 	    }, new FreeMarkerEngine());
 		
 		/**
-		 * Ruta del POST para el formulario de pruebas
+		 * Ruta del POST para el formulario de fabricante
 		 */
-		post("/form", (req, res) -> {
+		post("/manufacturer/new", (req, res) -> {
+			String result = "resultadoManufacturer.ftl";
 			Map<String, Object> attributes = new HashMap<String, Object>();
 			
 			// Se recuperan los parametros
 			String nombre = req.queryParams("nombre");
-			String texto = req.queryParams("texto");
-			String[] coches = req.queryParamsValues("coches");
-			String listaCoches = null;
-			if (coches != null) {
-				listaCoches = "";
-				for (String coche : coches) {
-					listaCoches += coche + " ";
-				}
-			}
+			String pais = req.queryParams("pais");
+
+			logger.info("nombre: " + nombre);
+			logger.info("pais: " + pais);
 			
-			// Se cargan en el modelo
+			Manufacturer manufacturer = new Manufacturer();
+			manufacturer.setName(nombre);
+			manufacturer.setCountry(pais);
+			
+			if (manufacturer.validate()) {
+		        attributes.put("textoCabecera", "Fabricante creado");
+		        attributes.put("result", "Fabricante creado correctamente.");
+				manufacturerDao.createManufacturer(manufacturer);
+			} else {
+		        attributes.put("textoCabecera", "Nuevo coche");
+		        attributes.put("errores", manufacturer.getErrors());
+		        result = "formularioManufacturer.ftl";
+			}
+
+			logger.info("result: " + result);
+			
+			// Se cargan en el modelo los parámetros
 			attributes.put("nombre", nombre);
-			attributes.put("texto", texto);
-			attributes.put("coches", listaCoches);
+			attributes.put("pais", pais);
 	
-	        return new ModelAndView(attributes, "resultado.ftl");
+	        return new ModelAndView(attributes, result);
 	    }, new FreeMarkerEngine());
 		
 		/**
 		 * Ruta del formulario para crear/editar coches
 		 */
-		get("/car", (req, res) -> {
+		get("/car/new", (req, res) -> {
 			Map<String, Object> attributes = new HashMap<String, Object>();
-			
-			attributes.put("fabricantes", manufacturerDao.findAll());
 
-	        return new ModelAndView(attributes, "car.ftl");
+	        attributes.put("textoCabecera", "Nuevo coche");
+	        attributes.put("manufacturers", manufacturerDao.findAll());
+
+	        return new ModelAndView(attributes, "formularioCar.ftl");
 	    }, new FreeMarkerEngine());
 		
 		/**
 		 * Ruta del POST para el formulario de pruebas
 		 */
-		post("/car", (req, res) -> {
+		post("/car/new", (req, res) -> {
+			String result = "resultadoCar.ftl";
 			Map<String, Object> attributes = new HashMap<String, Object>();
 			
 			// Se recuperan los parametros
 			String nombre = req.queryParams("nombre");
 			String fabricante = req.queryParams("fabricante");
+
+			logger.info("nombre: " + nombre);
+			logger.info("fabricante: " + fabricante);
 			
 			Manufacturer manufacturer = manufacturerDao.findById(new Integer(fabricante));
+			logger.info("manufacturer: " + manufacturer);
+			Car coche = new Car(null, nombre, manufacturer);
 			
-			carDao.createCar(new Car(null, nombre, manufacturer));
+			if (manufacturer.validate()) {
+		        attributes.put("textoCabecera", "Coche creado");
+		        attributes.put("result", "Coche creado correctamente.");
+				carDao.createCar(coche);
+			} else {
+		        attributes.put("textoCabecera", "Nuevo coche");
+		        attributes.put("errores", manufacturer.getErrors());
+		        result = "formularioManufacturer.ftl";
+			}
+
+			logger.info("result: " + result);
 			
 			// Se cargan en el modelo
-			attributes.put("result", "Coche creado.");
 			attributes.put("nombre", nombre);
-			attributes.put("fabricantes", manufacturerDao.findAll());
+			attributes.put("fabricante", manufacturer);
 	
-	        return new ModelAndView(attributes, "car.ftl");
+	        return new ModelAndView(attributes, result);
 	    }, new FreeMarkerEngine());
 		
 		/**
