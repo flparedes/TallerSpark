9c5dd7c520aa556e357d8014225b63be56111338
Francisco Luis Paredes <franciscoluis.paredes@gmail.com>
2017-03-23 15:44:53 +0100

    Insertar nuevo documento
    Se añade la función encargada de insertar un nuevo documento en Base de
    Datos.

--- MyMega/src/main/java/es/franl2p/services/DocumentService.java
+++ MyMega/src/main/java/es/franl2p/services/DocumentService.java
@@ -1,8 +1,13 @@
 package es.franl2p.services;
 
 import es.franl2p.dao.DocumentDao;
+import es.franl2p.dao.UserDao;
 import es.franl2p.model.Document;
+import es.franl2p.model.User;
+import spark.utils.IOUtils;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 
 import org.slf4j.Logger;
@@ -12,6 +17,7 @@ public class DocumentService {
 	private final static Logger logger = LoggerFactory.getLogger(DocumentService.class);
 
 	private DocumentDao documentDao = new DocumentDao();
+	private UserDao userDao = new UserDao();
 	
 	/**
 	 * 
@@ -22,5 +28,37 @@ public class DocumentService {
     	logger.info("Documents found: " + docs);
     	
     	return docs;
+    }
+    
+    /**
+     * Inserta el nuevo documento en la Base de Datos.
+     * @param fileName Nombre del documento a insertar.
+     * @param is InputStream con el contenido del fichero a insertar.
+     * @param userName Nombre del usuario que realiza la subida.
+     * @return Resultado de la inserción.
+     * @throws IOException Excepción en caso de que falle la lectura del contenido.
+     */
+    public boolean insert(Document documento) throws IOException {
+    	return documentDao.create(documento);
+    }
+    
+    /**
+     * Inserta el nuevo documento en la Base de Datos.
+     * @param fileName Nombre del documento a insertar.
+     * @param is InputStream con el contenido del fichero a insertar.
+     * @param userName Nombre del usuario que realiza la subida.
+     * @return Resultado de la inserción.
+     * @throws IOException Excepción en caso de que falle la lectura del contenido.
+     */
+    public boolean insert(String fileName, InputStream is, String userName) throws IOException {
+    	byte[] data = inputStream2ByteArray(is);
+    	User user = userDao.findByName(userName);
+    	return documentDao.create(new Document(null, fileName, data, user));
+    }
+    
+    private byte[] inputStream2ByteArray(InputStream is) throws IOException {
+    	byte[] data = IOUtils.toByteArray(is);
+    	is.close();
+    	return data;
     }
 }
